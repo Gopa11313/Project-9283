9a6a290c54681b9b82488cc5dc72718a38dafa39
gopa11313 <gopalaaa9819525993@gmail.com>
2023-01-23 00:06:48 -0500

    everything had been done expect some validation

--- app.js
+++ app.js
@@ -14,11 +14,19 @@ var movieArray = [
     year_Relased: "July 18, 2008",
     Description:
       "The plot follows the vigilante Batman, police lieutenant James Gordon, and district attorney Harvey Dent, who form an alliance to dismantle organized crime in Gotham City. Their efforts are derailed by the Joker, an anarchistic mastermind who seeks to test how far Batman will go to save the city from chaos.",
-    picture: "/assests/the_dark_knight.jpg",
-    showTimeSection: [{
-      date: "1/20/2023",  //  date: ["1/20/2023","1/20/2023","1/20/2023"],  //  length now 9 but with this its more correct
-      time: [  { time: "9am-12pm"},{ time: "1:30pm-4Pm"}, {time: "6pm-8pm" } ] ,
-    }], 
+    picture: "/assests/main.jpg",
+    rating: "4.01",
+    video: "https://www.youtube.com/embed/EXeTwQWrcwY",
+    showTimeSection: [
+      {
+        date: "1/20/2023", //  date: ["1/20/2023","1/20/2023","1/20/2023"],  //  length now 9 but with this its more correct
+        time: [
+          { time: "9am-12pm" },
+          { time: "1:30pm-4Pm" },
+          { time: "6pm-8pm" },
+        ],
+      },
+    ],
   },
   {
     name: "Tenet",
@@ -27,10 +35,18 @@ var movieArray = [
     Description:
       "Tintin, cartoon character, an intrepid young investigative reporter who stars in a series of popular Belgian comic book albums. Accompanied by his faithful fox terrier, Snowy (Milou in the original French), Tintin travels the world in the service of truth and justice.",
     picture: "/assests/tenet.jpg",
-    showTimeSection: [{
-      date: "2/20/2023",
-      time: [  { time: "9am-12pm"},{ time: "1:30pm-4Pm"}, {time: "6pm-8pm" } ] ,
-    }],
+    rating: "4.5",
+    video: "https://www.youtube.com/embed/AZGcmvrTX9M",
+    showTimeSection: [
+      {
+        date: "2/20/2023",
+        time: [
+          { time: "9am-12pm" },
+          { time: "1:30pm-4Pm" },
+          { time: "6pm-8pm" },
+        ],
+      },
+    ],
   },
   {
     name: "Venom",
@@ -39,10 +55,38 @@ var movieArray = [
     Description:
       "venom, the poisonous secretion of an animal, produced by specialized glands that are often associated with spines, teeth, stings, or other piercing devices. The venom apparatus may be primarily for killing or paralyzing prey or may be a purely defensive adaptation. Some venoms also function as digestive fluids.",
     picture: "/assests/movie3.jpeg",
-    showTimeSection: [{
-      date: "3/20/2023",
-      time: [  { time: "9am-12pm"},{ time: "1:30pm-4Pm"}, {time: "6pm-8pm" } ] ,
-    }],
+    rating: "3.01",
+    video: "https://www.youtube.com/embed/u9Mv98Gr5pY",
+    showTimeSection: [
+      {
+        date: "3/20/2023",
+        time: [
+          { time: "9am-12pm" },
+          { time: "1:30pm-4Pm" },
+          { time: "6pm-8pm" },
+        ],
+      },
+    ],
+  },
+  {
+    name: "The Witcher",
+    runTime: "1h 52m",
+    year_Relased: "October 5, 2018",
+    Description:
+      "The Witcher is a fantasy drama web television series created by Lauren Schmidt Hissrich for Netflix. It is based on the book series of the same name by Polish writer Andrzej Sapkowski. The Witcher follows the story of Geralt of Rivia, a solitary monster hunter, who struggles to find his place in a world where people often prove more wicked than monsters and beasts.",
+    picture: "/assests/main.jpgs",
+    rating: "6.5",
+    video: "https://www.youtube.com/embed/u9Mv98Gr5pY",
+    showTimeSection: [
+      {
+        date: "3/20/2023",
+        time: [
+          { time: "9am-12pm" },
+          { time: "1:30pm-4Pm" },
+          { time: "6pm-8pm" },
+        ],
+      },
+    ],
   },
 ];
 //funcation
@@ -54,13 +98,18 @@ const viewDetails = (evnt) => {
     var data = movieArray[i];
     if (clickedMovie == "The_Dark_Knight") {
       localStorage.setItem("clickedMovie", JSON.stringify(data));
-      console.log(
-        "We are here" + JSON.parse(localStorage.getItem("clickedMovie"))
-      );
+      console.log(JSON.parse(localStorage.getItem("clickedMovie")));
+      return true;
     }
     if (clickedMovie == "Tenet") {
+      localStorage.setItem("clickedMovie", JSON.stringify(data));
+      console.log(JSON.parse(localStorage.getItem("clickedMovie")));
+      return true;
     }
     if (clickedMovie == "Venom") {
+      localStorage.setItem("clickedMovie", JSON.stringify(data));
+      console.log(JSON.parse(localStorage.getItem("clickedMovie")));
+      return true;
     }
   }
 };
@@ -69,9 +118,10 @@ const viewDetails = (evnt) => {
 movieSection.addEventListener("click", viewDetails);
 
 function setData() {
-  const data = JSON.parse(localStorage.getItem("clickedMovie"));
+  // const data = JSON.parse(localStorage.getItem("clickedMovie"));
+  const data = movieArray[0];
   console.log("cliked Item:" + data.picture);
-  document.getElementById("coverImg").scr = data.picture;
+  document.getElementById("coverImg").src = data.picture;
   document.getElementById("description").innerHTML = data.Description;
   paypalEmailSection.style.display = "none";
   paypalPasswordSection.style.display = "none";
@@ -133,28 +183,43 @@ function calculate() {
   document.getElementById("total").innerHTML = "Total:$" + total;
 }
 
-
 function setDetails() {
-  console.log("sas " +movieArray[0].showTimeSection[0].date)
-  for(i=0;i<movieArray[0].showTimeSection[0].time.length;i++)
-    {
-      const data=movieArray[0].showTimeSection[0].time[i];
-      console.log("its length "+movieArray[0].showTimeSection[0].time.length)
-      var button = document.createElement('button');
-      button.innerHTML=data.time;
-      button.addEventListener('click', () => {
-        location.href='purchase.html'
-      
-      })
-      detailsDiv.appendChild(button);
-    }
+  detailsDiv.style.display = "flex";
+  detailsDiv.style.justifyContent = "space-evenly";
+  const runtime = document.getElementById("runtime");
+  const year_Relased = document.getElementById("yearReleased");
+
+  const rating = document.getElementById("rating");
+  // var data = JSON.parse(localStorage.getItem("clickedMovie"));
+  const data = movieArray[0];
+  const video = document.querySelector("iframe");
+  video.src = data.video;
+  video.setAttribute(
+    "allow",
+    "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+  );
+  runtime.innerHTML = "<span>Runtime: </span>" + data.runTime;
+  year_Relased.innerHTML = "<span>Year Released:</span>" + data.year_Relased;
+  rating.innerHTML = "<span>Rating:</span>" + data.rating;
+  document.getElementById("title").innerHTML = data.name;
+  document.getElementById("description").innerHTML = data.Description;
+
+  for (i = 0; i < movieArray[0].showTimeSection[0].time.length; i++) {
+    const data = movieArray[0].showTimeSection[0].time[i];
+
+    var button = document.createElement("button");
+    button.className = "detailsButton";
+    button.innerHTML = data.time;
+    button.addEventListener("click", () => {
+      location.href = "purchase.html";
+    });
+    detailsDiv.appendChild(button);
   }
+}
 
-  // href="detail.html">
+function mainSectionClick() {
+  localStorage.setItem("clickedMovie", JSON.stringify(movieArray[0]));
+}
 
-
-
-
-
-
-   
+function validation() {}
+// href="detail.html">
